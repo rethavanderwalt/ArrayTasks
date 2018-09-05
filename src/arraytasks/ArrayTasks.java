7f411187343fb33d8a847d820c6a96f92546bd01
Retha <91030418@mail.computerpower.ac.nz>
2018-09-06 09:02:17 +1200

    Task 1 & 2

--- src/arraytasks/ArrayTasks.java
+++ src/arraytasks/ArrayTasks.java
@@ -6,15 +6,23 @@ import java.util.Arrays;
 public class ArrayTasks {
 
     public static int gradesArraySize = 5;
-    
+     // TASK 2 //
+    public static int subjectArraySize = 3;
+    public static int newSubjectArraySize = 5;
     
     public static void main(String[] args) {
         char[] studentGrades = {'A', 'B', 'D', 'F'};
+        String[] studentSubjects = {"Maths", "PE", "Music"};
         
-        // display all values of 'array'
-        for (char element: studentGrades) {
-            System.out.println("Original grades: " + element);
+        
+         // TASK 1 //
+        // display all values of studentGrades
+        System.out.println("Original grades: ");
+        for (int i = 0; i< studentGrades.length; i++) {
+            System.out.println(studentGrades[i]);
         }
+        System.out.print("\n");
+
         
         // establishing the size of the new array
         char[] newStudentGrades = new char[gradesArraySize];
@@ -27,9 +35,35 @@ public class ArrayTasks {
         System.arraycopy(studentGrades, 2, newStudentGrades, 3, 2);
         
         // display new combined array
-        for (char element: newStudentGrades) {
-            System.out.println("New grades: " + element);
+        System.out.println("New grades: ");
+        for (int i = 0; i< newStudentGrades.length; i++) {
+            System.out.println(newStudentGrades[i]);
         }
-    }
-    
+        System.out.print("\n");
+
+        
+         // TASK 2 //
+        // show student subjects
+        System.out.println("Student subjects: ");
+        for (int i = 0; i< studentSubjects.length; i++) {
+            System.out.println(studentSubjects[i]);
+        }
+        System.out.print("\n");
+
+        // show second item of studentSubjects
+        System.out.println("Second subject is: " + studentSubjects[1] + "\n");
+        
+        // adding new array for new subjects
+        String[] newStudentSubjects = new String[newSubjectArraySize];
+        System.arraycopy(studentSubjects, 0, newStudentSubjects, 0, 3);
+        newStudentSubjects[3] = "Food tech";
+        newStudentSubjects[4] = "Social studies";
+        
+        // display new student subjects array
+       System.out.println("New student subjects: ");
+        for (int i = 0; i< newStudentSubjects.length; i++) {
+            System.out.println(newStudentSubjects[i]);
+        }
+
+    }    
 }
